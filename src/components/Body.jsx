e514bfb83063ea4569a8240adcc22a260575e759
Martin Nyaga <martinmugendi8@gmail.com>
2024-01-25 17:20:59 +0300

    added my Photo

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,6 +5,13 @@ const Body = () => {
                 Hello There, i'm Martin Nyaga
             </h1>
             <h2>A Passionate Fullstack Developer from Nairobi, Kenya</h2>
+
+            <img
+            src="/gitphoto.jpg"  
+            alt="My Photo"
+            style={{ width: '100%', height: 'auto' }}
+            />
+            
         </div>
     )
 }
